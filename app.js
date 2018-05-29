0a3d450e257e0774c8fe745341265974379e9676
servinlp <servin.nissen@gmail.com>
2018-05-29 15:48:03 +0200

    Node initial build

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = 	require( 'express' ),
+	ENV = 			require( 'dotenv' ).config().parsed,
+	bodyParser = 	require( 'body-parser' ),
+	moment = 		require( 'moment' ),
+	helmet = 		require( 'helmet' ),
+	compression =	require( 'compression' ),
+
+	app = 			express(),
+	PORT = 			ENV.NODE_ENV === 'production' ? ENV.PORT : ENV.DEV_PORT
+
+app.use( compression() )
+app.use( helmet() )
+
+app.use( express.static( 'public' ) )
+
+app.set( 'views', './views' )
+app.set( 'view engine', 'ejs' )
+
+app.use( bodyParser.urlencoded( { extended: false } ) )
+app.use( bodyParser.json() )
+
+app.get( '/', ( req, res ) => {
+
+	res.render( 'index' )
+
+} )
+
+app.get( '*', ( req, res ) => {
+
+	res.render( '404' )
+
+} )
+
+app.listen( PORT, err => {
+
+	console.log( `Hello from http://localhost:${ PORT }` )
+
+} )
