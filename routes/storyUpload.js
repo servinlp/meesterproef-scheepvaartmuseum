2bc588b46910c1ebe52f2f9342ee85995e5f06cd
servinlp <servin.nissen@gmail.com>
2018-06-07 10:22:08 +0200

    Mostly code styling stuff

--- routes/storyUpload.js
+++ routes/storyUpload.js
@@ -31,43 +31,43 @@ async function uploadToDb( data, dataFiles, res ) {
 	}
 	
 	// fileLocation inserts all the files and filetypes into the files table in the database
-	const fileLocation = dataFiles.map( (file) => {
+	const fileLocation = dataFiles.map( file => {
 		// fileMeta contains all the links and types of the current upload
 		const fileMeta = {
 			link: file.location,
 			type: file.mimetype
 		}
 		return pool.query( 'INSERT INTO files SET ?', fileMeta )
-	})
+	} )
 
 	// fileLocation is an array of queries
 	const fileQueries = await Promise.all( [
-		pool.query('INSERT INTO stories SET ?', storyMeta),
+		pool.query( 'INSERT INTO stories SET ?', storyMeta ),
 		...fileLocation
 	] )
 
 	// Gets the first item from the array and removes it
 	const story = fileQueries.shift()
 	// Here we concat all the ids of the files that were inserted into the database
-	const fileIds = fileQueries.reduce( (acc, val) => {
-		return acc.concat(val.insertId)
+	const fileIds = fileQueries.reduce( ( acc, val ) => {
+		return acc.concat( val.insertId )
 	}, [] )
 	// Makes a string of all the ids ['id1', 'id2']
-	const fileIdsString = fileIds.join(',')
+	const fileIdsString = fileIds.join( ',' )
 
 	// Adds the file ids to the story
-	const updateStory = await pool.query( 'UPDATE stories set ? where ID = ?', [ {
+	await pool.query( 'UPDATE stories set ? where ID = ?', [ {
 		files: fileIdsString
 	}, story.insertId ] )
 	
 	// Splits tags by comma or space
-	const storyTags = storyMeta.tags.split(/[ ,]+/);
+	const storyTags = storyMeta.tags.split( /[ ,]+/ )
 	// Makes an array of all the queries needed for adding tags
 	const storyTagQueries = storyTags.map( tag => {
 		return pool.query( 'SELECT * FROM tags WHERE name = ?', tag )
-	})
+	} )
 	// Returns an array of the results of storyTagQueries
-	const storyTagResults = await Promise.all(storyTagQueries)
+	const storyTagResults = await Promise.all( storyTagQueries )
 	
 	const tagIds = []
 
@@ -77,23 +77,23 @@ async function uploadToDb( data, dataFiles, res ) {
 	*/
 	for ( let i = 0; i < storyTagResults.length; i ++ ) {
 		const result = storyTagResults[ i ]
-		if(result.length > 0) {
+		if ( result.length > 0 ) {
 			// If the tag exists in the table already
-			tagIds.push(result[0].ID)
+			tagIds.push( result[ 0 ].ID )
 		}
 		else {
 			// If the tag DOESN'T exist in the table already
-			const insertedStoryTag = await pool.query('INSERT INTO tags SET ?', {
+			const insertedStoryTag = await pool.query( 'INSERT INTO tags SET ?', {
 				name: storyTags[i]
 			} )
 			// Add tags to the array
-			tagIds.push(insertedStoryTag.insertId)
+			tagIds.push( insertedStoryTag.insertId )
 		}
 	}
 	// Makes a pretty string of the tags
-	const tagIdsString = tagIds.join(',');
+	const tagIdsString = tagIds.join( ',' )
 	// Adds the tags to the stories in the database
-	const updateStoryTags = await pool.query( 'UPDATE stories set ? where ID = ?', [ {
+	await pool.query( 'UPDATE stories set ? where ID = ?', [ {
 		tags: tagIdsString
 	}, story.insertId ] )
 
