211506bacec4a03763e455a258280e3f03e1301e
RobinFrugte97 <robin.frugte@hotmail.com>
2018-06-12 13:23:33 +0200

    added back an import

--- views/index.js
+++ views/index.js
@@ -1,4 +1,5 @@
 import header from './components/header/header.js'
+import setUpMap from './components/map/map.js'
 import toggleToolButton from './components/storyAddItemButton/storyAddItemButton.js'
 
 ( function IIFE () {
