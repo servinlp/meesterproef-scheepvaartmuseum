0cbc6caa32821ddf6ca6f4b3a724fffe603923bf
meesrutten <mees.rutten@cobrasystems.nl>
2018-06-14 10:21:12 +0200

    Fix eslint and componentize storyGrid animation

--- views/components/pagination/pagination.js
+++ views/components/pagination/pagination.js
@@ -1,6 +1,5 @@
 function pagination(){
-	const currentPage = document.querySelector('[aria-current="page"]')
-	const allLinks = document.querySelectorAll('.stories__pagination .link')
+
 }
 
 export default pagination
--- views/index.js
+++ views/index.js
@@ -2,21 +2,15 @@ import header from './components/header/header.js'
 import setUpMap from './components/map/map.js'
 import toggleToolButton from './components/storyAddItemButton/storyAddItemButton.js'
 import { enhancedDetail, enhancedDetailInit } from './components/enhancedDetail/enhancedDetail.js'
+import animateStoryOverview from './components/storyGrid/storyGrid.js'
 
 ( function IIFE () {
 
 	if( !( document.documentElement.classList && document.querySelectorAll ) ) return
 
-	TweenMax.staggerFrom('header + *', 1, { autoAlpha: 0, y: -20 }, 0.1)
-	TweenMax.set(['[class*="card"] h2', '[class*="card"] p'], {autoAlpha: 0, y: -20} )
-	TweenMax.staggerFrom('[class*="card"]', 1, { delay: .4, autoAlpha: 0, x: -20, onComplete: () => {
-		TweenMax.staggerTo(['[class*="card"] h2', '[class*="card"] p'], 1, {autoAlpha: 1, y: 0, onComplete: () => {
-			TweenMax.set(['[class*="card"] h2', '[class*="card"] p', '[class*="card"]', 'header + *'], { clearProps: 'all' } )
-		}}, 0.1 )
-	} }, 0.2)
-
 	enhancedDetailInit()
 	header()
+	animateStoryOverview()
 	setUpMap()
 	toggleToolButton()
 	
