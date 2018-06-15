21b9241db0ce2679d53374f3e16ce52e1d3df206
meesrutten <meesrutten@gmail.com>
2018-06-15 16:49:09 +0200

    Add uploadform with naturallanguage,dutch content, possibility to add extra inputs, to do: styling

--- views/index.js
+++ views/index.js
@@ -1,5 +1,5 @@
 import header from './components/header/header.js'
-import uploadForm from './components/uploadForm/uploadForm.js'
+import { addTagSuggestions, progressiveDiscloseForm, uploadForm, autoScaleInput } from './components/uploadForm/uploadForm.js'
 import setUpMap from './components/map/map.js'
 import toggleToolButton from './components/storyAddItemButton/storyAddItemButton.js'
 import { enhancedDetailInit } from './components/enhancedDetail/enhancedDetail.js'
@@ -9,11 +9,14 @@ import animateStoryOverview from './components/storyGrid/storyGrid.js'
 
 	if( !( document.documentElement.classList && document.querySelectorAll ) ) return
 	
+	animateStoryOverview()
 	enhancedDetailInit()	
 	header()
 	uploadForm()
+	autoScaleInput()
+	progressiveDiscloseForm()
+	addTagSuggestions()
 	toggleToolButton()
-	animateStoryOverview()
 	setUpMap()
 	
 } )()
