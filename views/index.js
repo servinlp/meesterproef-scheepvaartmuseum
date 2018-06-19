633b2fe47e6c628ab957d7c52da272343aee1638
RobinFrugte97 <robin.frugte@hotmail.com>
2018-06-19 12:26:57 +0200

    Removed intersectionObserver containers

--- views/index.js
+++ views/index.js
@@ -4,17 +4,15 @@ import setUpMap from './components/map/map.js'
 import toggleToolButton from './components/storyAddItemButton/storyAddItemButton.js'
 import animateStoryOverview from './components/storyGrid/storyGrid.js'
 import autocompleteFromApiInit from './components/lib/autocomplete.js'
-import { enhancedDetailInit } from './components/enhancedDetail/enhancedDetail.js'
 
 ( function IIFE () {
 
 	if( !( document.documentElement.classList && document.querySelectorAll ) ) return
 	animateStoryOverview()
-	enhancedDetailInit()
 	header()
 	uploadForm()
 	toggleToolButton()
 	setUpMap()
-	autocompleteFromApiInit() 
-	
+	autocompleteFromApiInit()
+
 } )()
