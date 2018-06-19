85fc3ff1d46e379c5f3b64f4509225cfc73a6580
RobinFrugte97 <robin.frugte@hotmail.com>
2018-06-19 12:27:44 +0200

    changed naming

--- views/index.js
+++ views/index.js
@@ -4,11 +4,13 @@ import setUpMap from './components/map/map.js'
 import toggleToolButton from './components/storyAddItemButton/storyAddItemButton.js'
 import animateStoryOverview from './components/storyGrid/storyGrid.js'
 import autocompleteFromApiInit from './components/lib/autocomplete.js'
+import { selectComponents } from './components/enhancedDetail/enhancedDetail.js'
 
 ( function IIFE () {
 
 	if( !( document.documentElement.classList && document.querySelectorAll ) ) return
 	animateStoryOverview()
+	selectComponents()
 	header()
 	uploadForm()
 	toggleToolButton()
