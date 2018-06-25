bb28e06589eb09fbe7f42408c6c568659e90e747
meesrutten <meesrutten@gmail.com>
2018-06-25 22:32:49 +0200

    Add intersection animations and fix form animation

--- views/index.js
+++ views/index.js
@@ -6,12 +6,13 @@ import animateStoryOverview from './components/storyGrid/storyGrid.js'
 import autocompleteFromApiInit from './components/lib/autocomplete.js'
 import colorThief from './components/storyHighlight/storyHighlight.js'
 import { selectComponents } from './components/enhancedDetail/enhancedDetail.js'
-import {optimizedResize} from './components/lib/helpers.js'
+import {optimizedResize, animateOnIntersect} from './components/lib/helpers.js'
 
 
 ( function IIFE () {
 
 	if( !( document.documentElement.classList && document.querySelectorAll ) ) return
+	animateOnIntersect()
 	setUpMap()
 	animateStoryOverview()
 	selectComponents()
